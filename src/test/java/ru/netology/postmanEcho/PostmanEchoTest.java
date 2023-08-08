8b275683e1a1580aaf155ea79b880c5bb8caaf68
EM <jivova.ed@gmail.com>
2023-08-08 21:10:01 +0300

    init

---
+++ src/test/java/ru/netology/postmanEcho/PostmanEchoTest.java
@@ -0,0 +1,22 @@
+package ru.netology.postmanEcho;
+import org.junit.jupiter.api.Test;
+
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.equalTo;
+
+public class PostmanEchoTest {
+
+    @Test
+    void shouldReturnSendData() {
+        given()
+                .baseUri("https://postman-echo.com")
+                .body("Привет")
+
+                .when()
+                .post("/post")
+                .then()
+                .statusCode(200)
+                .body("data", equalTo("Привет!"))
+        ;
+    }
+}
